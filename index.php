973bb8e321c7a3b185a54d432b945cf353a1103a
Thomas Cottis <tomcottis21@gmail.com>
2022-03-20 16:18:29 -0600

    pushing clear header and navbar css

--- index.php
+++ index.php
@@ -33,41 +33,44 @@
     <button id="toggleDm" class="buttonDm bi bi-lamp"></button>
 
     <!-- -------------------- Nav bar -------------------- -->
-    <nav class="navbar navbar-expand-sm navbar-light">
-        <a href="#" class="navbar-brand mb-0 h1">Navbar</a>
+    <header class="fixed-top header-transparent">
+        <nav class="navbar navbar-expand-sm">
+            <a href="#" class="navbar-brand mb-0 h1">Navbar</a>
 
-        <button class="navbar-toggler" 
-        type="button" 
-        data-bs-toggle="collapse" 
-        data-bs-target="#navbarNav"
-        aria-controls="navbarNav"
-        aria-expanded="false"
-        aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
-                <li class="nav-item active">
-                    <a href="#" class="nav-link">Home</a>
-                </li>
-                <li class="nav-item active">
-                    <a href="#" class="nav-link">Item 1</a>
-                </li>
-                <li class="nav-item">
-                    <a href="#" class="nav-link">Item 2</a>
-                </li>
-                <li class="nav-item">
-                    <a href="#" class="nav-link">Item 3</a>
-                </li>
-                <li class="nav-item">
-                    <a href="#" class="nav-link">Item 4</a>
-                </li>
-                <li class="nav-item active">
-                    <a href="#" class="nav-link">Item 5</a>
-                </li>
-            </ul>
-        </div>
-    </nav>
+            <button class="navbar-toggler" 
+            type="button" 
+            data-bs-toggle="collapse" 
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item active">
+                        <a href="#" class="nav-link">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">Item 1</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">Item 2</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">Item 3</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">Item 4</a>
+                    </li>
+                    <li class="nav-item active">
+                        <a href="#" class="nav-link">Item 5</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </header>
     <!-- -------------------- Hero Section -------------------- -->
 <section id="hero">
     <div class="container-fluid p-0 hero-container">
