5c2ebc53c7f44def9417df0d1e52fd4cfad08817
Thomas Joseph Cottis <tomcottis21@gmail.com>
2022-04-03 22:27:57 -0600

    updated bootstrap cdn

--- index.php
+++ index.php
@@ -130,9 +130,10 @@
     <title>Thomas Cottis Website Design & Development</title>
     
     <!-- -------------------- Bootstrap CDN -------------------- -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.min.js"></script>
-
+    
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    
     <!-- -------------------- Bootstrap icons CDN link -------------------- -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
 
