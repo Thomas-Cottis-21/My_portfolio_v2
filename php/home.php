bf2a4b9944862deb4fdf61421111e2046d197d56
Thomas Joseph Cottis <tomcottis21@gmail.com>
2022-04-19 18:38:30 -0600

    nested foreach to access nested arrays of data

--- php/home.php
+++ php/home.php
@@ -5,6 +5,31 @@
         header("Location: /index.php");
         exit;
     }
+
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "test";
+
+    try {
+        $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        echo "<script>console.log('Database connection successfull -> Access Granted')</script>";
+
+        $stmt = $conn->prepare('SELECT * FROM clients');
+
+        $stmt->execute();
+
+        $clientInfo = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    } catch(PDOException $error) {
+        echo "<script>console.log('ERROR: " . addslashes($error->getMessage()) . "')</script>";
+        echo "<script>console.log('Database connection failed -> Access Denied')</script>";
+    }
+
+    $conn = null;
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -21,6 +46,18 @@
     </div>
     <div>
         <h3>We'll, we have a lot of work to do <?=$_SESSION["name"]?>!</h3>
+    </div>
+    <div>
+        <?php 
+            foreach ($clientInfo as $arrayLarge) {
+                foreach ($arrayLarge as $arraySmall) {
+                    echo "<p>$arraySmall</p>";
+                }
+            }
+        ?>
+    </div>
+    <div>
+    <!-- <p><?=$clientInfo[1]["clientId"] . " " . $clientInfo[1]["first_name"] . " " . $clientInfo[1]["last_name"] . " " . $clientInfo[1]["email"] . " " . $clientInfo[1]["number"] . " " . $clientInfo[1]["message"] . " " . $clientInfo[1]["date"]?></p> -->
     </div>
 </body>
 </html>
