13df8655239aede89e82aa3f656035f5fb6d8638
Thomas Joseph Cottis <tomcottis21@gmail.com>
2022-05-10 23:11:08 -0600

    db to localhost

--- php/databaseConnection.php
+++ php/databaseConnection.php
@@ -1,11 +1,11 @@
 <?php
 
-$servername = "localhost";   
+/* $servername = "localhost";   
 $username = "homasan5_thomas";
 $password = "JohanaRamirez21$$";
-$dbname = "homasan5_portfolio_clients_database";
+$dbname = "homasan5_portfolio_clients_database"; */
 
-/* $servername = "localhost";
+$servername = "localhost"; // bluehost says box5294
 $username = "root";
 $password = "";
-$dbname = "my_portfolio_v2_test"; */
+$dbname = "my_portfolio_v2_test";
